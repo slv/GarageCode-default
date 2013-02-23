79e08c21e559d7e141200dd1bb5396420b3d8cef
Michele Salvini <info@slv.dj>
2013-02-23 18:32:41 +0100

    Added Memcached Sessions

--- info.php
+++ info.php
@@ -1,6 +1,9 @@
+<?php session_start(); ?>
 <pre>
   SERVER_ADDR: <?=$_SERVER['SERVER_ADDR']?><br />
-</pre>
-<pre>
   REMOTE_ADDR: <?=$_SERVER['REMOTE_ADDR']?><br />
+</pre>
+
+<pre>
+  numero di visite: <?php $_SESSION['counter'] = (int)@$_SESSION['counter'] + 1; echo $_SESSION['counter']; ?>
 </pre>
